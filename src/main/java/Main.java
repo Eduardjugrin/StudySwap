ae9d9b9b1c9f3ed2b9c93d8dc97622cd3f31740c
Eduardjugrin <jugrineduard@gmail.com>
2024-01-05 15:05:53 +0100

    aggiunta javafx

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,35 @@
-package org.example;
-
-import Model.Buyer;
 import Model.Note;
 import Model.NoteContainer;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
 
 import java.util.List;
 
-public class Main {
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Hello World!");
+
+        Button btn = new Button("Say 'Hello World'");
+        btn.setOnAction(e -> System.out.println("Hello World!"));
+
+        StackPane root = new StackPane();
+        root.getChildren().add(btn);
+
+        primaryStage.setScene(new Scene(root, 300, 250));
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+    /*
+
+
     static NoteContainer noteContainer = new NoteContainer();
     public static void main(String[] args) {
         Note note1 = new Note("titolo1", "content1", 1);
@@ -23,6 +46,5 @@ public class Main {
             System.out.println("Utente ID: " + note.getUserId());
             System.out.println("----");
         }
-
-    }
+*/
 }
